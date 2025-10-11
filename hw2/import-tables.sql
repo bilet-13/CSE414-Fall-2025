772c865ee0722e3cff555fdc3f8d2f2c5ca154a1
bilet-13 <r10922114@csie.ntu.edu.tw>
2025-10-11 15:22:56 -0700

    feat: adjust the csv files position and import-tables.sql to meet auto grader

--- hw2/import-tables.sql
+++ hw2/import-tables.sql
@@ -1,6 +1,6 @@
 .mode csv
-.import --skip 1 ./flights-csv/carriers.csv Carriers
-.import --skip 1 ./flights-csv/aircraft_types.csv Aircraft_Types
-.import --skip 1 ./flights-csv/n_numbers.csv N_Numbers
-.import --skip 1 ./flights-csv/cancellation_codes.csv Cancellation_Codes
-.import --skip 1 ./flights-csv/flights-2024-09.csv Flights
+.import --skip 1 carriers.csv Carriers
+.import --skip 1 aircraft_types.csv Aircraft_Types
+.import --skip 1 n_numbers.csv N_Numbers
+.import --skip 1 cancellation_codes.csv Cancellation_Codes
+.import --skip 1 flights-2024-09.csv Flights
